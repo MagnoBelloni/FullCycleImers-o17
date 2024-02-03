2e83d7a3f3dfcaa24f04c04ced1d66fe9c6b964c
Magno Belloni <bellonidesouza6@gmail.com>
2024-02-03 16:00:37 -0300

    nestjs-api

---
+++ Imersao/nestjs-api/src/orders/dto/create-order.dto.ts
@@ -0,0 +1,35 @@
+import { Type } from 'class-transformer';
+import { IsInt } from 'class-validator';
+import { IsPositive } from 'class-validator';
+import { isNotEmpty } from 'class-validator';
+import {
+    ArrayNotEmpty,
+    IsNotEmpty,
+    IsString,
+    MaxLength,
+    ValidateNested,
+} from 'class-validator';
+
+export class CreateOrderDto {
+    @ArrayNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => OrderItemDto)
+    items: OrderItemDto[];
+
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    card_hash: string;
+}
+
+export class OrderItemDto {
+    @IsPositive()
+    @IsInt()
+    @IsNotEmpty()
+    quantity: number;
+
+    @MaxLength(36)
+    @IsString()
+    @IsNotEmpty()
+    product_id: string;
+}
